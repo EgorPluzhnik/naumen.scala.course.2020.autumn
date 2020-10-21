d8bf2ffd117b4e813926433dbc657f3f465b4ea3
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-10-21 15:35:51 +0500

    add EpsilonCompare

--- homeworks/homework_2/src/test/scala/Extensions.scala
+++ homeworks/homework_2/src/test/scala/Extensions.scala
@@ -1,3 +1,8 @@
+
 object Extensions {
 
+  implicit class EpsilonCompare(val number: Double) {
+
+    def ~(sample: Double)(implicit epsilon: Double): Boolean = (number - sample).abs < epsilon
+  }
 }
