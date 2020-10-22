ed401f747eb63cf450f14a33d0393fb42f3de232
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-10-22 15:20:20 +0500

    implement prettyBooleanFormatter

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -4,10 +4,25 @@ import utest._
 
 object Test extends TestSuite{
 
-    val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+    val tests: Tests = Tests{
+        def prettyBooleanFormatter(): Unit = {
+            assert(Exercises.prettyBooleanFormatter1(true) == "правда")
+            assert(Exercises.prettyBooleanFormatter1(false) == "ложь")
+            assert(Exercises.prettyBooleanFormatter1(1) == "1")
+            assert(Exercises.prettyBooleanFormatter1(0.754) == "0.754")
+            assert(Exercises.prettyBooleanFormatter1("string") == "string")
+            assert(Exercises.prettyBooleanFormatter1(Seq(1, 2, 3, 4, 5)) == "List(1, 2, 3, 4, 5)")
+            assert(Exercises.prettyBooleanFormatter1(Seq(1.2, 2.3, 3.4, 4.5, 5.6)) == "List(1.2, 2.3, 3.4, 4.5, 5.6)")
+        }
+        'test_prettyBooleanFormatter1 - {
+            prettyBooleanFormatter()
+        }
+        'test_prettyBooleanFormatter2 - {
+            prettyBooleanFormatter()
+        }
+
+        'test_prettyBooleanFormatter3 - {
+            prettyBooleanFormatter()
         }
     }
 }
