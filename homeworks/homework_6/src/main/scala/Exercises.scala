76a7e0da4082aba599a55473402428619f45663b
Egor Pluzhnik <egor.pluzhnik.lol@gmail.com>
2020-12-15 13:25:49 +0500

    done homework 6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,27 +1,47 @@
+import scala.util.matching.Regex
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse;
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
    *
-   * @param idx
-   * @return
+   * @param idx number index (starts from 0)
+   * @return fibonacci number
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = idx match {
+    case 0 | 1 => idx
+    case _ => fibonacci4Index(idx -1) + fibonacci4Index(idx -2)
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    def fibs(a: Int = 0, b: Int = 1): Stream[Int] = Stream.cons(a, fibs(b, a+b))
+
+    fibs() take idx + 1
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String =
+    text.map(char => MORSE.getOrElse(char.toString.toUpperCase(), char.toString))
+      .mkString(" ")
+      .trim
 
 
-  def wordReverse(text: String): String = ???
+  private val WordPattern: Regex = """\b\p{L}+\b""".r
 
+  def wordReverse(text: String): String =
+    WordPattern.replaceAllIn(text, word => reverseWithCapitalize(word.toString()))
+
+
+  private def reverseWithCapitalize(word: String): String = word.head.isUpper match {
+    case true => word.reverse.toLowerCase.capitalize
+    case false => word.reverse
+  }
 }
--- homeworks/homework_6/src/test/scala/Test.scala
+++ homeworks/homework_6/src/test/scala/Test.scala
@@ -12,22 +12,35 @@ object Test extends TestSuite{
     'fibonacci4Index - {
       assert(Exercises.fibonacci4Index(2) == 1)
       assert(Exercises.fibonacci4Index(5) == 5)
+      assert(Exercises.fibonacci4Index(6) == 8)
+      assert(Exercises.fibonacci4Index(9) == 34)
+      assert(Exercises.fibonacci4Index(10) == 55)
+      assert(Exercises.fibonacci4Index(0) == 0)
+      assert(Exercises.fibonacci4Index(20) == 6765)
+      assert(Exercises.fibonacci4Index(30) == 832040)
+
     }
 
     'fibonacci - {
       assert(Exercises.fibonacci(2) == Seq(0, 1, 1))
       assert(Exercises.fibonacci(5) == Seq(0, 1, 1, 2, 3, 5))
+      assert(Exercises.fibonacci(9) == Seq(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
     }
 
     'morse - {
       assert(Exercises.morse("SOS") == "... --- ...")
-      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -..!")
+      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -.. !")
     }
 
     'wordReverse - {
       assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
+      assert(Exercises.wordReverse("Hello world!") == "Olleh dlrow!")
+      assert(Exercises.wordReverse("Hello мир world!") == "Olleh рим dlrow!")
+      assert(Exercises.wordReverse("") == "")
+      assert(Exercises.wordReverse("Заказ шалаш топот") == "Заказ шалаш топот")
+      assert(Exercises.wordReverse("имя") == "ями")
+      assert(Exercises.wordReverse("коронавирус 2020") == "суриванорок 2020")
+      assert(Exercises.wordReverse("745 hello world 123") == "745 olleh dlrow 123")
     }
-
-
   }
 }
